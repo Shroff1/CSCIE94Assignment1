2f7ece558ddbffb1828f1bd928c5ae32876d4f66
Aniket Shroff <aniketmanoj@hotmail.com>
2019-02-12 23:58:01 -0500

    Comiting

---
+++ assignment1/Controllers/ticController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ticTacToeLogic;
+
+namespace assignment1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ticController : ControllerBase
+    {
+        [HttpPost]
+        public void Post([FromBody] char[] game)
+        {
+            logicEngineBase input = new logicEngineBase()
+            {
+                gameBoardResponse = game
+            };
+
+            logicEngine engineOne = new logicEngine();
+
+            engineOne.ExecuteMove(input.gameBoardResponse);
+
+
+        
+
+
+        }
+    }
+}
---
+++ assignment1/ticTacToeLogic/logicEngineBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ticTacToeLogic
+{
+    /*Create a class with the following tempalte. 
+         * { "move": 7, 
+         *   "azurePlayerSymbol": "X", 
+         *   "winner": "X", 
+         *   "winPositions": [ 1, 4, 7 ], 
+         *   "gameBoard":[ "O", "X", "O", "?", "X", "O", "?", "X", "?" ] 
+         *   }
+    */
+    public class logicEngineBase
+    {
+            public int move { get; set; }
+            public char azurePlayerSymbol { get; set; }
+            public char winner { get; set; }
+            public int[] winPositions { get; set; }
+            public char[] gameBoardResponse { get; set;}
+    }
+}
