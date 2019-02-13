a92d97cf62d3c0a77598210235cc4fcb5729dcc7
Aniket Shroff <aniketmanoj@hotmail.com>
2019-02-13 03:36:30 -0500

    commit 336am

--- assignment1/Startup.cs
+++ assignment1/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,25 +10,68 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 
 namespace assignment1
 {
     public class Startup
     {
+        /// <summary>
+        /// Defines the configuration of the Web API applicaiton
+        /// </summary>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// Gets the hosting environment.
+        /// </summary>
+        /// <value>
+        /// The hosting environment.
+        /// </value>
+        public IHostingEnvironment HostingEnvironment { get; }
+
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        /// <value>
+        /// The configuration.
+        /// </value>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">The services.</param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
+        /// <summary>
+        /// SWAGGER: Configures the swagger UI.
+        /// </summary>
+        /// <param name="swaggerGenOptions">The swagger gen options.</param>
+
+        private void ConfigureSwaggerUI(SwaggerGenOptions swaggerGenOptions)
+        {
+            swaggerGenOptions.SwaggerDoc("v1", new Info { Title = "TicTacToe", Version = "v1" });
+
+            var filePath = Path.Combine(HostingEnvironment.ContentRootPath, "TicTacToeSwagger.config");
+            swaggerGenOptions.IncludeXmlComments(filePath);
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// Configures the specified application.
+        /// </summary>
+        /// <param name="app">The application.</param>
+        /// <param name="env">The env.</param>
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,6 +80,34 @@ namespace assignment1
             }
 
             app.UseMvc();
+
+            // SWAGGER: Insert middleware to expose the generated Swagger as JSON endpoints
+            app.UseSwagger(c =>
+            {
+                c.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
+                {
+                    // Necessary for API management so it has the proper values for the Backend service URL otherwise 
+                    // you will see an error in trace similar to "Backend service URL is not defined"
+                    swaggerDoc.Host = httpReq.Host.Value;
+                });
+            });
+
+            // SWAGGER: swagger-ui-middleware to expose interactive documentation
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TicTacToe Swagger Demo");
+            });
+
         }
+
+
+
+
+
+
+
+
+
+
     }
 }
